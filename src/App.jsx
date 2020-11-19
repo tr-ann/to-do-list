4e65575c6af4cd01a5a4ef0c4732a5154c3b1a20
Ann Trepachko <trepachko.ann@gmail.com>
2020-11-19 18:43:44 +0300

    moved store provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { ToDoList } from './components/ToDoList';
-import store from './store/store';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <header className="App-header">
-          <ToDoList />
-        </header>
-      </Provider>
+      <header className="App-header">
+        <ToDoList />
+      </header>
     </div>
   );
 }
